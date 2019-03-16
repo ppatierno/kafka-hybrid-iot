62f7529f86f4be4b6fc542f0b19fc8176da4fca2
Paolo Patierno <ppatierno@live.com>
2019-03-16 14:25:41 +0100

    Added command logic via AMQP to the control application Added AMQP receiver on the Node-RED IoT gateway Updated NodeRED flows

--- control-app/src/main/java/kafka/iot/Control.java
+++ control-app/src/main/java/kafka/iot/Control.java
@@ -10,7 +10,14 @@ import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.kafka.client.common.TopicPartition;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
+import io.vertx.proton.ProtonClient;
+import io.vertx.proton.ProtonConnection;
+import io.vertx.proton.ProtonHelper;
+import io.vertx.proton.ProtonSender;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.qpid.proton.amqp.messaging.Rejected;
+import org.apache.qpid.proton.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,19 +31,26 @@ public class Control {
     private static final String SEEK = "seek";
     private static final String SEEK_TO_BEGIN = "seektobegin";
     private static final String STATUS = "status";
+    private static final String CONTROL = "control";
 
     private static final String BOOTSTRAP_SERVERS = "BOOTSTRAP_SERVERS";
     private static final String CONSUMER_GROUPID = "CONSUMER_GROUPID";
     private static final String CONSUMER_TOPIC = "CONSUMER_TOPIC";
     private static final String CONSUMER_AUTO_OFFSET_RESET = "CONSUMER_AUTO_OFFSET_RESET";
+    private static final String AMQP_SERVER = "AMQP_SERVER";
+    private static final String AMQP_ADDRESS = "AMQP_ADDRESS";
 
     private static final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
     private static final String DEFAULT_CONSUMER_GROUPID = "consumer-app";
     private static final String DEFAULT_CONSUMER_TOPIC = "iot-temperature-max";
     private static final String DEFAULT_CONSUMER_AUTO_OFFSET_RESET = "earliest";
+    private static final String DEFAULT_AMQP_SERVER = "localhost:5672";
+    private static final String DEFAULT_AMQP_ADDRESS = "iot-control";
 
 
     private static Set<TopicPartition> assignedTopicPartitions;
+
+    private static ProtonSender sender;
 
     public static void main(String[] args) {
 
@@ -44,6 +58,8 @@ public class Control {
         String groupId = System.getenv().getOrDefault(CONSUMER_GROUPID, DEFAULT_CONSUMER_GROUPID);
         String topic = System.getenv().getOrDefault(CONSUMER_TOPIC, DEFAULT_CONSUMER_TOPIC);
         String autoOffsetReset = System.getenv().getOrDefault(CONSUMER_AUTO_OFFSET_RESET, DEFAULT_CONSUMER_AUTO_OFFSET_RESET);
+        String amqpServer = System.getenv().getOrDefault(AMQP_SERVER, DEFAULT_AMQP_SERVER);
+        String amqpAddress = System.getenv().getOrDefault(AMQP_ADDRESS, DEFAULT_AMQP_ADDRESS);
 
         Vertx vertx = Vertx.vertx();
 
@@ -110,6 +126,37 @@ public class Control {
                         vertx.eventBus().publish("status", String.format("Joining group = [%s] ...", groupId));
                     }
                     break;
+
+                case CONTROL:
+                    
+                    log.info("Sending control command...");
+                    Message msg = ProtonHelper.message(amqpAddress, "Control!");
+                    sender.send(msg, delivery -> {
+                        log.info("Message delivered {}", delivery.getRemoteState());
+                        if (delivery.getRemoteState() instanceof Rejected) {
+                            Rejected rejected = (Rejected) delivery.getRemoteState();
+                            log.info("... but rejected {} {}", rejected.getError().getCondition(), rejected.getError().getDescription());
+                        }
+                    });
+                    break;
+            }
+        });
+        
+        log.info("Connecting via AMQP to {}", amqpServer);
+        ProtonClient client = ProtonClient.create(vertx);       
+
+        String[] amqpServerParts = amqpServer.split(":");
+        client.connect(amqpServerParts[0], Integer.valueOf(amqpServerParts[1]), done -> {
+
+            if (done.succeeded()) {
+                log.info("Connected via AMQP");
+                ProtonConnection conn = done.result();
+                conn.open();
+
+                sender = conn.createSender(amqpAddress);
+                sender.open();
+            } else {
+                log.error("Error connecting via AMQP", done.cause());
             }
         });
     }
